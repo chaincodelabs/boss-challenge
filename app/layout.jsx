ea1e6efebca45d86567df2735e0b0fb2c0aa314c
Jonas <jonas@chaincode.com>
2025-12-01 16:47:33 -0500

    add open graph image

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,18 @@ import './globals.css';
 
 export const metadata = {
   title: '₿OSS Challenge',
-  description: '₿OSS Challenge – start your career in bitcoin open source',
+  description: '₿OSS Challenge - start your career in bitcoin open source',
+  openGraph: {
+    title: '₿OSS Challenge',
+    description: '₿OSS Challenge - start your career in bitcoin open source',
+    images: ['/img/boss-challenge.png'],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '₿OSS Challenge',
+    description: '₿OSS Challenge - start your career in bitcoin open source',
+    images: ['/img/boss-challenge.png'],
+  },
 };
 
 export default function RootLayout({ children }) {
